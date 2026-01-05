844ec3d55a2348559f1b69ae1ad7a9a6fc272ca7
sayeduzzaman <sayeduzzamansajid@gmail.com>
2026-01-06 01:42:21 +0600

    Secure API keys using environment variables

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAWSeUsKQOA0JV6c3F4KAysuhaSalGXW3s",
-  authDomain: "veridian-bills.firebaseapp.com",
-  projectId: "veridian-bills",
-  storageBucket: "veridian-bills.firebasestorage.app",
-  messagingSenderId: "705191550469",
-  appId: "1:705191550469:web:dd9c020ef6fe55b932c66c"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
